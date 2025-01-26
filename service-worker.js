26aebcefd74a24f3bb6c682a0bb134badf575c9b
Jaswanth Ponnusamy <54830245+JaswanthPonnusamy-SP@users.noreply.github.com>
2025-01-26 18:04:23 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,21 @@ const ASSETS_TO_CACHE = [
     '/IGCaption/',
     '/IGCaption/index.html',
     'https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,700,900',
+    'https://fonts.gstatic.com/s/montserrat/v29/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2',
     'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.1/TweenMax.min.js',
   '/IGCaption/igcs/js/main-js.js',
     '/IGCaption/igcs/css/cssanimation.css',
-  '/IGCaption/igcs/css/main.css'
+  '/IGCaption/igcs/css/main.css',
+    'https://jaswanthponnusamy-sp.github.io/IGCaption/igcs/images/Like.png',
+    '/IGCaption/igcs/images/heart.png',
+    '/IGCaption/igcs/images/angry.png',
+    '/IGCaption/igcs/images/Haha_face.png',
+    '/IGCaption/igcs/images/Sad_face.png',
+    '/IGCaption/igcs/images/Wow_face.png',
+    '/IGCaption/igcs/images/rect-500.svg',
+    '/IGCaption/igcs/images/hand.svg',
+    'IGCaption/igcs/mp4/final.mp4'
 ];
 
 self.addEventListener('install', (event) => {
