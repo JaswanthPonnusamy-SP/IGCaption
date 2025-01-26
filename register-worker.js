e7b4e264a1fda90abb443d731c0bd9bd6b4dbf03
Jaswanth Ponnusamy <54830245+JaswanthPonnusamy-SP@users.noreply.github.com>
2025-01-26 17:57:03 +0530

    Update and rename register-worker to register-worker.js

--- register-worker.js
+++ register-worker.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/service-worker.js')
+            .register('/IGCaption/service-worker.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
